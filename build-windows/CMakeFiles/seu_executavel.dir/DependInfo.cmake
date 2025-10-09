7ccfe957194858f2b1bd73a3796d2c063391d11e
Racciatti <enzonracciatti@gmail.com>
2025-10-09 18:50:49 -0300

    add windows config and build

---
+++ build-windows/CMakeFiles/seu_executavel.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/racciatti/Desktop/Code/trabalho-cg/src/canvas.c" "CMakeFiles/seu_executavel.dir/src/canvas.c.o" "gcc" "CMakeFiles/seu_executavel.dir/src/canvas.c.o.d"
+  "/home/racciatti/Desktop/Code/trabalho-cg/src/graphics.c" "CMakeFiles/seu_executavel.dir/src/graphics.c.o" "gcc" "CMakeFiles/seu_executavel.dir/src/graphics.c.o.d"
+  "/home/racciatti/Desktop/Code/trabalho-cg/src/matrix.c" "CMakeFiles/seu_executavel.dir/src/matrix.c.o" "gcc" "CMakeFiles/seu_executavel.dir/src/matrix.c.o.d"
+  "/home/racciatti/Desktop/Code/trabalho-cg/deps/imgui/backends/imgui_impl_sdl2.cpp" "CMakeFiles/seu_executavel.dir/deps/imgui/backends/imgui_impl_sdl2.cpp.o" "gcc" "CMakeFiles/seu_executavel.dir/deps/imgui/backends/imgui_impl_sdl2.cpp.o.d"
+  "/home/racciatti/Desktop/Code/trabalho-cg/deps/imgui/backends/imgui_impl_sdlrenderer2.cpp" "CMakeFiles/seu_executavel.dir/deps/imgui/backends/imgui_impl_sdlrenderer2.cpp.o" "gcc" "CMakeFiles/seu_executavel.dir/deps/imgui/backends/imgui_impl_sdlrenderer2.cpp.o.d"
+  "/home/racciatti/Desktop/Code/trabalho-cg/deps/imgui/imgui.cpp" "CMakeFiles/seu_executavel.dir/deps/imgui/imgui.cpp.o" "gcc" "CMakeFiles/seu_executavel.dir/deps/imgui/imgui.cpp.o.d"
+  "/home/racciatti/Desktop/Code/trabalho-cg/deps/imgui/imgui_draw.cpp" "CMakeFiles/seu_executavel.dir/deps/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/seu_executavel.dir/deps/imgui/imgui_draw.cpp.o.d"
+  "/home/racciatti/Desktop/Code/trabalho-cg/deps/imgui/imgui_tables.cpp" "CMakeFiles/seu_executavel.dir/deps/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/seu_executavel.dir/deps/imgui/imgui_tables.cpp.o.d"
+  "/home/racciatti/Desktop/Code/trabalho-cg/deps/imgui/imgui_widgets.cpp" "CMakeFiles/seu_executavel.dir/deps/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/seu_executavel.dir/deps/imgui/imgui_widgets.cpp.o.d"
+  "/home/racciatti/Desktop/Code/trabalho-cg/src/main.cpp" "CMakeFiles/seu_executavel.dir/src/main.cpp.o" "gcc" "CMakeFiles/seu_executavel.dir/src/main.cpp.o.d"
+  "/home/racciatti/Desktop/Code/trabalho-cg/src/ui.cpp" "CMakeFiles/seu_executavel.dir/src/ui.cpp.o" "gcc" "CMakeFiles/seu_executavel.dir/src/ui.cpp.o.d"
+  "" "seu_executavel" "gcc" "CMakeFiles/seu_executavel.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
